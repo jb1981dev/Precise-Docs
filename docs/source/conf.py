0b073746cfdc7a8d64cf2cf082e903b6adaaafb3
jb1981dev <jmjbackx@gmail.com>
2025-09-25 09:22:01 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,8 +2,8 @@
 
 # -- Project information
 
-project = 'Instance Wrangler'
-copyright = '2025, Pro Art Tools'
+project = 'Precise Tools Documentation'
+copyright = '2025, Precise Tools'
 author = 'JB'
 
 release = '1.0'
